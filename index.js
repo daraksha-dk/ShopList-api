aae6c7aaa781fdf89047b19e721fd93597f9ab9e
daraksha-dtg <daraksha.atg@gmail.com>
2022-06-01 16:55:05 +0530

    make it better

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello to Shoplist Api");
+});
+
 const PORT = process.env.PORT || 8080;
 mongoose
   .connect(
