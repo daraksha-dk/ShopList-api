515b6294014bda0594218b21bbe75998aa89980c
daraksha-dtg <daraksha.atg@gmail.com>
2022-06-12 00:41:32 +0530

    commit

--- index.js
+++ index.js
@@ -17,15 +17,16 @@ app.get("/", (req, res) => {
   res.send("Hello to Shoplist Api");
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.kx4uz.mongodb.net/?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.xwx3a.mongodb.net/?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }
   )
+
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
   )
